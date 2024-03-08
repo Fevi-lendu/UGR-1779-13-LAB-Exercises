83efa114ec4cd68021e3343008646e08a9151829
Fevi-lendu <150247854+Fevi-lendu@users.noreply.github.com>
2024-03-08 14:29:45 -0800

    exercise1

---
+++ LAB-1-Variables-and-types.dart
@@ -0,0 +1,9 @@
+void main() {
+  String name = "feven";
+  int age = 3;
+  String favoriteColor = "Blue";
+
+  print("Name: $name");
+  print("Age: $age");
+  print("Favorite Color: $favoriteColor");
+}
